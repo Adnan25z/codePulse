000aaa42a1a5c65f6337ce40a900f4680e7454cd
Adnan <adnanzafar2001@gmail.com>
2024-04-17 23:05:49 +1000

    create category list component and routing

--- UI/codepulse/src/app/app-routing.module.ts
+++ UI/codepulse/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin/categories',
+    component: CategoryListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- UI/codepulse/src/app/app.module.ts
+++ UI/codepulse/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,
