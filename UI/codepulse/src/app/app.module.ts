0c150e809c94fcb09cb6f4ef5d14b94cdb7908f1
Adnan <adnanzafar2001@gmail.com>
2024-04-17 23:31:58 +1000

    created add category component and used two way data binding

--- UI/codepulse/src/app/app.module.ts
+++ UI/codepulse/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
+import { AddCategoryComponent } from './features/category/add-category/add-category.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CategoryListComponent
+    CategoryListComponent,
+    AddCategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
