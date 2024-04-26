335e5fd4214893be752638124f1078b24ff4de92
Adnan <adnanzafar2001@gmail.com>
2024-04-26 13:14:32 +1000

    created service method and dsiplayed single category

--- UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
+++ UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-edit-category',
@@ -10,16 +12,34 @@ import { Subscription } from 'rxjs';
 export class EditCategoryComponent implements OnInit, OnDestroy {
   id: string | null = null;
   paramsSubscription?: Subscription;
-  constructor(private route: ActivatedRoute){
+  category?: Category;
+
+  constructor(private route: ActivatedRoute,
+    private categoryService: CategoryService){
   }
 
   ngOnInit(): void {
     this.paramsSubscription = this.route.paramMap.subscribe({
       next:(params) =>{
         this.id = params.get('id');
+        if(this.id){
+          //get data from API for this category id
+          this.categoryService.getCategoryById(this.id)
+          .subscribe({
+            next:(response) =>{
+              this.category = response;
+            }
+          });
+        }
       }
     });
   }
+
+onFormSubmit():void{
+  console.log("done");
+  console.log(this.category);
+}
+
   ngOnDestroy(): void {
     this.paramsSubscription?.unsubscribe();
   }
--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -19,4 +19,8 @@ export class CategoryService {
   getAllCategories() : Observable<Category[]>{
     return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`);
   }
+
+  getCategoryById(id: string) : Observable<Category>{
+    return this.http.get<Category>(`${environment.apiBaseUrl}/api/categories/${id}`);
+  }
 }
