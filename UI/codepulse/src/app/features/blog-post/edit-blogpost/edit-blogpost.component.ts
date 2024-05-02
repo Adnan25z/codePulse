e1dbbad6d65d3081cd6d439fd45bade2e24bbf37
Adnan <adnanzafar2001@gmail.com>
2024-05-02 13:33:01 +1000

    UI Delete blogpost button working

--- UI/codepulse/src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ UI/codepulse/src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -22,6 +22,7 @@ export class EditBlogpostComponent implements OnInit, OnDestroy{
   routeSubscription?: Subscription;
   updateBlogPostSubscription?: Subscription;
   getBlogPostSubscription?: Subscription;
+  deleteBlogPostSubscription?: Subscription;
 
   constructor(private route: ActivatedRoute, private blogPostService: BlogPostService, private categoryService: CategoryService, private router: Router){
 
@@ -70,10 +71,21 @@ export class EditBlogpostComponent implements OnInit, OnDestroy{
     }
   }
 
+  onDelete(): void{
+    if(this.id){
+      //call service and delete blogpost
+      this.deleteBlogPostSubscription = this.blogPostService.deleteBlogPost(this.id).subscribe({
+        next: (response) => {
+          this.router.navigateByUrl('/admin/blogposts');
+        }
+      });
+    }
+  }
+
   ngOnDestroy(): void {
     this.routeSubscription?.unsubscribe();
     this.updateBlogPostSubscription?.unsubscribe();
     this.getBlogPostSubscription?.unsubscribe();
+    this.deleteBlogPostSubscription?.unsubscribe();
   }
-
 }
--- UI/codepulse/src/app/features/blog-post/services/blog-post.service.ts
+++ UI/codepulse/src/app/features/blog-post/services/blog-post.service.ts
@@ -24,10 +24,14 @@ export class BlogPostService {
   }
 
   getBlogPostById(id: string): Observable<BlogPost>{
-    return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}` );
+    return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`);
   }
 
   updateBlogPost(id: string, updatedBlogPost: UpdateBlogPost): Observable<BlogPost>{
     return this.http.put<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`, updatedBlogPost);
+  }
+
+  deleteBlogPost(id: string) : Observable<BlogPost>{
+    return this.http.delete<BlogPost>(`${environment.apiBaseUrl}/api/blogposts/${id}`);
   }
 }
