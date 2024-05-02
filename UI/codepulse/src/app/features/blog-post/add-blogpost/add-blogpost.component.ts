c9ee2a8ef706e3f144b0c1d0b73259341fe57bc9
Adnan <adnanzafar2001@gmail.com>
2024-05-02 10:38:22 +1000

    display categories in multiselect dropdown and capture categories

--- UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { BlogPostService } from '../services/blog-post.service';
+import { CategoryService } from '../../category/services/category.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Category } from '../../category/models/category.model';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.css']
 })
-export class AddBlogpostComponent {
+export class AddBlogpostComponent implements OnInit{
   model: AddBlogPost;
-  constructor(private blogPostServce: BlogPostService, private router: Router){
+  categories$?: Observable<Category[]>;
+  constructor(private blogPostService: BlogPostService, private router: Router, private categoryService: CategoryService){
     this.model = {
       title: '',
       shortDescription: '',
@@ -19,12 +23,15 @@ export class AddBlogpostComponent {
       featuredImageUrl: '',
       author: '',
       isVisible: true,
-      publishedDate: new Date()
+      publishedDate: new Date(),
     }
+  }
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
   }
 
   onFormSubmit(): void {
-    this.blogPostServce.createBlogPost(this.model)
+    this.blogPostService.createBlogPost(this.model)
     .subscribe({
       next: (response) => {
         this.router.navigateByUrl('/admin/blogposts');
