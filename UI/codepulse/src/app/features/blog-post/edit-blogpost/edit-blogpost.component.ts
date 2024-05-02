dd798289128bd7216f5db4343f931b8c87add768
Adnan <adnanzafar2001@gmail.com>
2024-05-02 14:17:27 +1000

    changed image selector as modal popup

--- UI/codepulse/src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ UI/codepulse/src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -17,6 +17,7 @@ export class EditBlogpostComponent implements OnInit, OnDestroy{
   model?: BlogPost;
   categories$?: Observable<Category[]>
   selectedCategories?: string[];
+  isImageSelectorVisible : boolean = false;
 
 
   routeSubscription?: Subscription;
@@ -80,6 +81,14 @@ export class EditBlogpostComponent implements OnInit, OnDestroy{
         }
       });
     }
+  }
+
+  openImageSelector(): void {
+    this.isImageSelectorVisible = true;
+  }
+
+  closeImageSelector(): void {
+    this.isImageSelectorVisible = false;
   }
 
   ngOnDestroy(): void {
