162808d731786a2550506df8d707a27e0bdaf7cb
Adnan <adnanzafar2001@gmail.com>
2024-05-05 00:00:34 +1000

    fetch all images and display working with minor tweaks

--- UI/codepulse/src/app/shared/components/image-selector/image-selector.component.ts
+++ UI/codepulse/src/app/shared/components/image-selector/image-selector.component.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ImageService } from './image.service';
+import { Observable } from 'rxjs';
+import { BlogImage } from '../../models/blog-image.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-image-selector',
   templateUrl: './image-selector.component.html',
   styleUrls: ['./image-selector.component.css']
 })
-export class ImageSelectorComponent {
+export class ImageSelectorComponent implements OnInit{
   private file?: File;
   fileName: string = '';
   title: string = '';
+  images$?: Observable<BlogImage[]>;
 
-  /**
-   *
-   */
+  @ViewChild('form', {static: false}) imageUploadForm?: NgForm;
+
   constructor(private imageService: ImageService) {
+  }
+
+  ngOnInit(): void {
+     this.getImages();
   }
 
   onFileUploadChange(event: Event): void{
@@ -28,9 +35,14 @@ export class ImageSelectorComponent {
       this.imageService.uploadImage(this.file, this.fileName, this.title)
       .subscribe({
         next: (response) => {
-          console.log(response);
+          this.imageUploadForm?.resetForm();
+          this.getImages();
         }
       });
     }
+  }
+
+  private getImages(){
+    this.images$ = this.imageService.getAllImages(); 
   }
 }
