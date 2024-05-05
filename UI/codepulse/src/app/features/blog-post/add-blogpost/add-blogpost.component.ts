cd46289ad06d147b6690b0cf94ffef160354a34f
Adnan <adnanzafar2001@gmail.com>
2024-05-05 22:31:15 +1000

    image selector component added in add blogpost component

--- UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ UI/codepulse/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { BlogPostService } from '../services/blog-post.service';
 import { CategoryService } from '../../category/services/category.service';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Category } from '../../category/models/category.model';
+import { ImageService } from 'src/app/shared/components/image-selector/image.service';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.css']
 })
-export class AddBlogpostComponent implements OnInit{
+export class AddBlogpostComponent implements OnInit, OnDestroy{
   model: AddBlogPost;
+  isImageSelectorVisible : boolean = false;
   categories$?: Observable<Category[]>;
-  constructor(private blogPostService: BlogPostService, private router: Router, private categoryService: CategoryService){
+
+  imageSelectorSubscription?: Subscription;
+  constructor(private blogPostService: BlogPostService, 
+    private router: Router, 
+    private categoryService: CategoryService,
+    private imageService: ImageService){
     this.model = {
       title: '',
       shortDescription: '',
@@ -27,8 +34,16 @@ export class AddBlogpostComponent implements OnInit{
       categories: []
     }
   }
+  
   ngOnInit(): void {
     this.categories$ = this.categoryService.getAllCategories();
+    this.imageSelectorSubscription = this.imageService.onSelectImage()
+    .subscribe({
+      next: (selectedImage) => {
+        this.model.featuredImageUrl = selectedImage.url;
+        this.closeImageSelector();
+      }
+    })
   }
 
   onFormSubmit(): void {
@@ -39,5 +54,17 @@ export class AddBlogpostComponent implements OnInit{
         this.router.navigateByUrl('/admin/blogposts');
       }
     });
+  }
+
+  openImageSelector(): void {
+    this.isImageSelectorVisible = true;
+  }
+
+  closeImageSelector(): void {
+    this.isImageSelectorVisible = false;
+  }
+
+  ngOnDestroy(): void {
+    this.imageSelectorSubscription?.unsubscribe();
   }
 }
