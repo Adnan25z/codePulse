3ec7372e2a32eda9be88a0b19603583ad5f8a70e
Adnan <adnanzafar2001@gmail.com>
2024-05-06 22:27:37 +1000

    created blog details component

---
+++ UI/codepulse/src/app/features/public/blog-details/blog-details.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-blog-details',
+  templateUrl: './blog-details.component.html',
+  styleUrls: ['./blog-details.component.css']
+})
+export class BlogDetailsComponent implements OnInit{
+  url: string | null = null;
+
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.route.paramMap
+    .subscribe({
+      next: (params) => {
+        this.url = params.get('url');
+      }
+    });
+    //fetch blog details by url
+    
+  }
+}
