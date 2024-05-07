6d00491829d27a830b5566a1b73a4495b199ee3d
Adnan <adnanzafar2001@gmail.com>
2024-05-07 16:49:32 +1000

    GET blogDetailsByUrl method implemented however some issues with route will be fixed in the next few commits

--- codePulse.API/Controllers/BlogPostsController.cs
+++ codePulse.API/Controllers/BlogPostsController.cs
@@ -102,7 +102,7 @@ namespace codePulse.API.Controllers
 
         // GET: {apibaseurl}/api/blogposts/{id}
         [HttpGet]
-        [Route("{id=Guid}")]
+        [Route("{id= guid}")]
         public async Task<IActionResult> GetBlogPostbyId([FromRoute] Guid id)
         {
             //Get the blogpost from repo
@@ -131,6 +131,42 @@ namespace codePulse.API.Controllers
                     UrlHandle = x.UrlHandle
                 }).ToList()
             };
+            return Ok(response);
+        }
+
+        // GET: {apibaseurl}/api/blogPosts/{urlhandle}
+        [HttpGet]
+        [Route("handle/{urlHandle}")]
+        public async Task<IActionResult> GetBlogPostByUrlHandle([FromRoute] string urlHandle)
+        {
+            // Get blogpost details from repository
+            var blogPost = await blogPostRepository.GetByUrlHandleAsync(urlHandle);
+
+            if (blogPost is null)
+            {
+                return NotFound();
+            }
+
+            // Convert Domain Model to DTO
+            var response = new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Author = blogPost.Author,
+                Content = blogPost.Content,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                IsVisible = blogPost.IsVisible,
+                PublishedDate = blogPost.PublishedDate,
+                ShortDescription = blogPost.ShortDescription,
+                Title = blogPost.Title,
+                UrlHandle = blogPost.UrlHandle,
+                Categories = blogPost.Categories.Select(x => new CategoryDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    UrlHandle = x.UrlHandle
+                }).ToList()
+            };
+
             return Ok(response);
         }
 
