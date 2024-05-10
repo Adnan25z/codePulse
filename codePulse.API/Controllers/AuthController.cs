89e8097a361c661e4d57a3d7a1bef1b6b32791ae
Adnan <adnanzafar2001@gmail.com>
2024-05-10 20:58:07 +1000

    created AuthController and Register Action Method

---
+++ codePulse.API/Controllers/AuthController.cs
@@ -0,0 +1,67 @@
+﻿using codePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace codePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        //POST: {apbaseurl}/api/auth/register
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto request)
+        {
+            // Create the IdentityUser Object
+            var user = new IdentityUser
+            {
+                UserName = request.Email?.Trim(),
+                Email = request.Email?.Trim()
+            };
+
+            // Create User
+            var identityResult = await userManager.CreateAsync(user, request.Password);
+            if(identityResult.Succeeded)
+            {
+                // Add role to user (Reader)
+                identityResult = await userManager.AddToRoleAsync(user, "Reader");
+
+                if(identityResult.Succeeded) 
+                {
+                    return Ok();
+                }
+                else
+                {
+                    if (identityResult.Errors.Any())
+                    {
+                        foreach (var error in identityResult.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if(identityResult.Errors.Any()) 
+                { 
+                    foreach(var error in identityResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return ValidationProblem(ModelState);
+        }
+    }
+}
