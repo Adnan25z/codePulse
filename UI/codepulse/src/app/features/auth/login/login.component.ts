8db1e32afd89f2a9f2b49885338e96ed22979416
Adnan <adnanzafar2001@gmail.com>
2024-05-12 20:36:44 +1000

    used package ngx-cookie-service to store jwt token

--- UI/codepulse/src/app/features/auth/login/login.component.ts
+++ UI/codepulse/src/app/features/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { LoginRequest } from '../models/login-request.model';
 import { AuthService } from '../services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,9 @@ import { AuthService } from '../services/auth.service';
 export class LoginComponent {
   model: LoginRequest;
 
-  constructor(private authService: AuthService){
+  constructor(private authService: AuthService,
+    private cookieService: CookieService,
+    private router: Router) {
     this.model = {
       email: '',
       password: ''
@@ -21,7 +25,12 @@ export class LoginComponent {
     this.authService.login(this.model)
     .subscribe({
       next: (response) => {
-        console.log(response);
+        // Set Auth Cookie
+        this.cookieService.set('Authorization', `Bearer ${response.token}`,
+          undefined, '/', undefined, true, 'Strict');
+
+          // Redirect back to Home
+          this.router.navigateByUrl('/');
       }
     });
   }
