bae17f26de1e750ab29be0eec4c516f4c025a903
Adnan <adnanzafar2001@gmail.com>
2024-05-13 22:29:44 +1000

    Implemented Logout functionality in UI

--- UI/codepulse/src/app/core/components/navbar/navbar.component.ts
+++ UI/codepulse/src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/features/auth/models/user.model';
 import { AuthService } from 'src/app/features/auth/services/auth.service';
 
 @Component({
@@ -7,8 +9,10 @@ import { AuthService } from 'src/app/features/auth/services/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  user?: User;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+    private router: Router) {
     
   }
 
@@ -16,9 +20,16 @@ export class NavbarComponent implements OnInit {
     this.authService.user()
     .subscribe({
       next: (response) => {
-        console.log(response);
+        this.user = response;
       }
     });
+
+    this.user = this.authService.getUser();
+  }
+
+  onLogout(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
