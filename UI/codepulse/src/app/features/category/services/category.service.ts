aa0692e4f5b1b0e4443911a8e17dc3778d967881
Adnan <adnanzafar2001@gmail.com>
2024-05-14 21:56:36 +1000

    passed JWT Token with API calls

--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -5,17 +5,15 @@ import { HttpClient } from '@angular/common/http';
 import { Category } from '../models/category.model';
 import { environment } from 'src/environments/environment';
 import { UpdateCategoryRequest } from '../models/update-category-request.model';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor(private http: HttpClient) { }
-
-  addCategory(model: AddCategoryRequest): Observable<void>{
-    return this.http.post<void>(`${environment.apiBaseUrl}/api/categories`, model);
-  }
+  constructor(private http: HttpClient,
+    private cookieService: CookieService) { }
 
   getAllCategories() : Observable<Category[]>{
     return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`);
@@ -25,9 +23,17 @@ export class CategoryService {
     return this.http.get<Category>(`${environment.apiBaseUrl}/api/categories/${id}`);
   }
 
+  addCategory(model: AddCategoryRequest): Observable<void>{
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/categories`, model);
+  }
+
   updateCategory(id:string, updateCategoryRequest: UpdateCategoryRequest) :
   Observable<Category>{
-    return this.http.put<Category>(`${environment.apiBaseUrl}/api/categories/${id}`, updateCategoryRequest);
+    return this.http.put<Category>(`${environment.apiBaseUrl}/api/categories/${id}`, updateCategoryRequest, {
+      headers: {
+        'Authorization': this.cookieService.get('Authorization')
+      }
+    });
   }
 
   deleteCategory(id: string): Observable<Category>{
